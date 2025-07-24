5d78c54d421fb42e2129324db7619bb14bc5a991
srivarthan12 <sri2112045@gmail.com>
2025-07-24 22:26:26 +0530

    Update paymentModel.js

--- server/models/paymentModel.js
+++ server/models/paymentModel.js
@@ -1,8 +1,7 @@
 // server/models/paymentModel.js
 import mongoose from 'mongoose';
 
-const paymentSchema = mongoose.Schema(
-  {
+const paymentSchema = mongoose.Schema({
     employee: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
     month: { type: String, required: true },
     year: { type: Number, required: true },
@@ -10,12 +9,7 @@ const paymentSchema = mongoose.Schema(
     advanceDeduction: { type: Number, required: true, default: 0 },
     bataPaid: { type: Number, required: true, default: 0 },
     finalPaid: { type: Number, required: true },
-  },
-  {
-    timestamps: true,
-  }
-);
+}, { timestamps: true });
 
 const Payment = mongoose.model('Payment', paymentSchema);
-
 export default Payment;
