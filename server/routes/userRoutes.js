2ff56ee0dff4537df45ccb1a649b39025094e6c6
srivarthan12 <sri2112045@gmail.com>
2025-07-24 21:03:54 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -13,21 +13,13 @@ import {
 import { protect, admin, superadmin } from '../middleware/authMiddleware.js';
 import upload from '../middleware/uploadMiddleware.js';
 
-// A user must be a logged-in admin to get all users or register a new one
 router.route('/').get(protect, admin, getUsers);
 router.route('/register').post(protect, admin, upload.single('photo'), registerUser);
-
-// Public login route
 router.post('/login', authUser);
-
-// A user must be a logged-in superadmin to delete another user
 router.route('/:id').delete(protect, superadmin, deleteUser);
-
-// New Routes for bata and salary
 router.route('/:id/bata').post(protect, admin, addUserBata);
 router.route('/:id/salary').put(protect, admin, raiseUserSalary);
-
-// New Route for detailed history
 router.route('/:id/financials').get(protect, admin, getUserFinancialDetails);
 
 export default router;
+
