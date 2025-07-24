778de5a04e03575d6e9c74262082c67e68cb0c39
srivarthan12 <sri2112045@gmail.com>
2025-07-24 20:53:19 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
   });
 } else {
-  app.get('/', (req, res) => {
+  app.get(/(.*)/, (req, res) => {
     res.send('API is running...');
   });
 }
